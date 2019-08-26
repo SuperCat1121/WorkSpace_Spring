5733764b7e28d0ec9a89c7f85bfbb5acb58085ac
이태식 <dlxotlr99@naver.com>
2019-08-26 15:53:46 +0900

    8월 26일

---
+++ Spring06_AOP/src/com/test01/Man.java
@@ -0,0 +1,15 @@
+package com.test01;
+
+public class Man {
+
+	void classWork() {
+		System.out.println("출석카드 찍는다.");
+		try {
+			System.out.println("컴퓨터 켜서 웹툰본다.");
+		} catch (Exception e) {
+			System.out.println("쉬는 날이다.");
+		} finally {
+			System.out.println("집에 간다.");
+		}
+	}
+}
