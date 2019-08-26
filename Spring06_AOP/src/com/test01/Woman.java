a3b5c7d4de5c543970d3dd6e586101123c17df47
이태식 <dlxotlr99@naver.com>
2019-08-26 15:58:49 +0900

    8월 26일

--- Spring06_AOP/src/com/test01/Woman.java
+++ Spring06_AOP/src/com/test01/Woman.java
@@ -1,11 +1,12 @@
 package com.test01;
 
-public class Man {
+public class Woman {
 
 	void classWork() {
-		System.out.println("출석카드 찍는다."); // ccc (cross-cutting concern)
+		System.out.println("출석카드 찍는다."); // ccc (공통 관심사항)
+		
 		try {
-			System.out.println("컴퓨터 켜서 웹툰본다."); // cc(core concern)
+			System.out.println("컴퓨터 켜서 쇼핑한다."); // cc (핵심 관심사항)
 		} catch (Exception e) {
 			System.out.println("쉬는 날이다.");
 		} finally {
